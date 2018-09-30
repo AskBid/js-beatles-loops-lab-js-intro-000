436df4c83d160cdb313b663e15b97f063dfd4b4d
sergio marelli <sergio.mrll@gmail.com>
2018-09-30 09:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function johnLennonFacts(array) {
+	var newarr = []
+    var i
+    for (i = 0; i < array.length; ++i) {
+        newarr.push(array[i] + "!!!");
+    }
+    return newarr
+}
